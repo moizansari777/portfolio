6762e0cf9462634b71f85179776ed6f55da22f5f
moizansari22 <113978056+moizansari22@users.noreply.github.com>
2024-06-25 22:41:49 +0500

    bugs(fixed): earth canvas motion fixed

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,8 +6,12 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>
-      
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
     </div>
   );
 };
